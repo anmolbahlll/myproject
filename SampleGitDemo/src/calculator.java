ab3386761fe4eb9c15e84961d59a17dc39faf6a7
anmolbahlll <anmolbahllll@gmail.com>
2021-03-22 17:20:24 +0530

    First commit

---
+++ SampleGitDemo/src/calculator.java
@@ -0,0 +1,12 @@
+
+public class calculator {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+	 }
+	 public static int add(int num1, int num2){
+	 return num1+num2;
+
+	}
+
+}
